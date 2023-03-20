c354afd4dfc3a3d380960ad2e842c49d394c2cd7
Héctor J. Vásquez <me@vasen.dev>
2023-03-20 13:51:28 -0600

    feat: update according to one pace json update

--- one_pace_updater.py
+++ one_pace_updater.py
@@ -11,47 +11,56 @@ plex_url = os.environ['PLEX_URL']
 plex_token = os.environ['PLEX_TOKEN']
 plex = PlexServer(plex_url, plex_token)
 
-one_pace_req = requests.get('https://api.onepace.net//list_progress_episodes.php')
-one_pace_data = one_pace_req.json()
-
-arcs_episodes = {}
-for episode in one_pace_data['episodes']:
-    if f'{episode["arc_id"]}' not in arcs_episodes:
-        arcs_episodes[f'{episode["arc_id"]}'] = []
-    arcs_episodes[f'{episode["arc_id"]}'].append(episode)
+one_pace_req = requests.get('https://onepace.net/_next/data/8398imKOhghFpoEoI3UZB/en/watch.json')
+one_pace_data = one_pace_req.json()['pageProps']['arcs']
 
 plex_show = plex.library.section('Anime').get('One Pace')
 plex_show.uploadArt('https://pbs.twimg.com/media/EUolT2OWsAYzRM8?format=jpg&name=large')
 plex_show.uploadPoster('https://dg31sz3gwrwan.cloudfront.net/poster/335179/1317675-4-optimized.jpg')
 
-for season, arc in enumerate(one_pace_data['arcs']):
+for season, arc in enumerate(one_pace_data, 1):
     try:
         plex_season = plex_show.season(season)
     except plexapi.exceptions.NotFound:
         print('S%02d: %s Not Found' % (season, arc['title']))
         continue
 
-    poster_url = f'https://onepace.net/_next/image?url=%2Fimages%2Farcs%2Fcover-{"-".join(arc["title"].lower().split(" "))}-arc_405w.webp&w=828&q=75'
-    plex_season.uploadPoster(poster_url)
-    plex_season.uploadArt(poster_url)
+    if arc['images'][0]:
+        poster_url = f"https://onepace.net/_next/image?url=%2Fimages%2Farcs%2F{arc['images'][0]['src']}&w=828&q=75"
+        plex_season.uploadPoster(poster_url)
+        plex_season.uploadArt(poster_url)
+
+    translation = {}
+    for t in arc['translations']:
+        if t['language']['code'] == 'en':
+            translation = t
+
     plex_season.edit(**{
         'title.value': arc['title'],
         'summary.value': (
-            f'Chapters: {arc["chapters"]}\n'
-            f'Episodes: {arc["episodes"]}'
+            f'Anime Episodes: {arc["anime_episodes"]} | '
+            f'Manga Chapters: {arc["manga_chapters"]}\n'
+            f"{translation['description']}"
         ),
     })
 
-    for episode in arcs_episodes[f'{arc["id"]}']:
+    for episode in arc['episodes']:
         try:
             plex_episode = plex_season.episode(episode['part'])
         except plexapi.exceptions.NotFound:
             print('S%02d E%02d: %s - %s Not Found' % (season, episode['part'], arc['title'], episode['title']))
             continue
+
+        translation = {}
+        for t in episode['translations']:
+            if t['language']['code'] == 'en':
+                translation = t
+
         plex_episode.edit(**{
-            'title.value': episode['title'],
+            'title.value': translation['title'],
             'summary.value': (
-                f'Chapters: {episode["chapters"]}\n'
-                f'Episodes: {episode["episodes"]}'
+                f'Anime Episodes: {episode["anime_episodes"]} | '
+                f'Manga Chapters: {episode["manga_chapters"]}\n'
+                f"{translation['description']}"
             ),
         })
